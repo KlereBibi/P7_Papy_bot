212eb8ca5bb54b2d8e7afce5bee6b782dd6848fb
Claire <biedermann.claire@gmail.com>
2021-12-09 15:48:19 -0500

    'essai'

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+from models.entities.inputparser import InputParser
+
+
+app = Flask(__name__)
+
+intpars = InputParser()
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'GET':
+        return 'pouet'
+    else:
+        var = request.form['query_user']
+        words = intpars.parser(var)
+        return words
+    
+    
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'errors/404.html'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
+
+
---
+++ models/entities/inputparser.py
@@ -0,0 +1,28 @@
+import unicodedata
+from settings.setting import STOPWORDS
+
+
+class InputParser:
+    
+    def parser(self, user_choice):
+        
+        inp_low = user_choice.lower()
+        
+        searched_word = []
+        
+        for element in inp_low:
+            if element not in STOPWORDS: 
+                searched_word.append(element)
+                
+        str_word = ' '.join(searched_word)
+        
+        word_no_acc = unicodedata.normalize('NFKD', inp_low).encode('ASCII', 'ignore').decode('ascii')
+        
+        pars = word_no_acc.split(" ")
+        
+        print(pars)
+        
+  
+inputparser = InputParser()
+user_choice = "Bonjour grand py, je cherche la rue de paradis à paris."
+inputparser.parser(user_choice)
