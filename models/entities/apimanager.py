12d576ca85c47e2ce0bf05e4c33db5bee4ef78d3
Claire <biedermann.claire@gmail.com>
2021-12-14 16:58:10 -0500

    coordonnée

--- models/entities/apimanager.py
+++ models/entities/apimanager.py
@@ -1,15 +1,23 @@
-import urllib.request
+import requests
 import json
+
 import sys
-print(sys.path)
+import os
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from entities.coord import Coord
 
 class ApiManager:
     
     def apistreet(self):
-        res = urllib.request.urlopen("http://open.mapquestapi.com/nominatim/v1/search.php?key=F3SzABt6KmK3CyhsZPFa96ucMZ3APqGT&format=json&postalcode=93470&state=France").read()
-        page = res.decode("utf8")
-        a = json.loads(page)
+        res = requests.get("http://open.mapquestapi.com/nominatim/v1/search.php?key=F3SzABt6KmK3CyhsZPFa96ucMZ3APqGT&format=json&postalcode=93470&state=France")
+        read = json.loads(res.text)
         
+        for element in read:
+            place = Coord(element['lat'], element['lon'])
+            
+        return place
 
 apimanager = ApiManager()
 apimanager.apistreet()
---
+++ models/entities/coord.py
@@ -0,0 +1,6 @@
+class Coord:
+
+    def __init__(self, lat, lon):
+
+        self.latitude = lat
+        self.longitude = lon
