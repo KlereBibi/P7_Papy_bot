d023626eb5e7a6383292b650b3e907d54b93c48e
Claire <biedermann.claire@gmail.com>
2022-01-10 13:50:36 -0500

    ça fonctionne

--- view/app.py
+++ view/app.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, request, jsonify
 import sys
 import os
 
+from controller.control import control
+
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
@@ -17,3 +19,4 @@ def ajax():
     user_text = request.form["userText"]
     print(user_text)
     return jsonify(["pouet"])
+    
