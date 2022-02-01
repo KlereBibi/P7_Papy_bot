2aae5152c53fc4e8efc3b8138a4318a7fec20bc7
Claire <biedermann.claire@gmail.com>
2022-02-01 15:37:47 -0500

    test selenium ok

--- tests_fonctional/test_user.py
+++ tests_fonctional/test_user.py
@@ -1,29 +1,33 @@
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-# Créer une session Firefox
-s = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=s)
-driver.maximize_window()
+def test_fonctionnal_application():
+    # Créer une session Firefox
+    s = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=s)
+    driver.maximize_window()
+    driver.implicitly_wait(30)
 
-# Appeler l’application web
-driver.get("http://127.0.0.1:5000/")
+    # Appeler l’application web
+    driver.get("http://127.0.0.1:5000/")
 
-# Localiser la zone de texte
-search_field = driver.find_element(By.ID, "userText")
-search_field.clear()
+    # Localiser la zone de texte
+    search_field = driver.find_element(By.ID, "userText")
+    search_field.clear()
 
-# Saisir et confirmer le mot-clé
-search_field.send_keys("Claire")
-search_field.submit()
+    # Saisir et confirmer le mot-clé
+    search_field.send_keys("Claire")
+    search_field.submit()
 
+    response = driver.find_element(By.CLASS_NAME, "papy").text
 
-response = driver.find_element(By.ID, "answer")
+    expected_value = "Mes petites cellules grises se souviennent que: Le Claire est une ville située dans le comté de Scott, dans l’État de l’Iowa, aux États-Unis. Lors du recensement de 2010, sa population s’élevait à 3 765 habitants."
 
-print(response)
+    assert response == expected_value
 
-# Fermer la fenêtre du navigateur
-driver.quit()
+    # Fermer la fenêtre du navigateur
+    
